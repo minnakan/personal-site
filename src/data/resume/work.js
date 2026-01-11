70a14b69ee4f140d251b37e0635c4fa08e4cfedf
K U Minnakan Seral <minnakan@gmail.com>
2026-01-11 13:10:41 -0800

    Update work.js

--- src/data/resume/work.js
+++ src/data/resume/work.js
@@ -25,14 +25,14 @@ const work = [
     position: 'Software Engineer - Unreal Engine & Backend ',
     url: 'https://www.geopogo.com/',
     startDate: '2025-01-01',
-    endDate: '2025-07-10',
+    endDate: '2025-08-10',
     summary: '3D urban design platform integrating Cesium and AI-assisted rendering for architects and planners.',
   },
   {
     name: 'ESRI',
     position: 'Product Engineer',
     url: 'https://developers.arcgis.com/qt/',
-    startDate: '2025-07-11',
+    startDate: '2025-08-11',
     summary: 'ArcGIS Maps SDK for Qt.',
   },
 ];
